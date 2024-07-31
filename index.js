4c50eea0efc850313fb3d44dccfcdb511640c1d5
iancorbett <iancorbett1324@gmail.com>
2024-07-31 10:22:16 -0600

    Update index.js
    adding comments

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function checkAllBoxes() {
 
 }
 
-
-
+// This function checks to see if all the input fields were filled out, if they were not,
+//then an alert will pop up asking for them to fill it out.
 
 /*const blogPost1 = {
     Username: 'IanMcLaren',
@@ -44,7 +44,20 @@ if (localStorage.getItem('formSubmissionslist') === null) {
     localStorage.setItem('blogPost3', JSON.stringify(blogPost3));
 }*/
 
+// all of the commented out code was previously used for hard coding the blog posts 
+//on the blog page.
+
 let forms = JSON.parse(localStorage.getItem('formSubmissionsList')) || [];
+
+//this was the one line of code i was initially missing, before receiving help from a tutor.
+// I had previously only being setting forms to equal an empty array
+// every time I refreshed the browser the localStorage would clear
+// and every time I would enter a new submission it would replace the previous one.
+// writing it this way allows us to store the previously submitted forms in the forms array
+//beforehand, it would clear local storage evrytime, and set forms to a new EMPTY array,
+//but now it takes the submission, parses it, and stores it in the forms array. 
+//This happens every time now, so the array can keep growing with more submissions, and not
+//have a cap of one form. 
 
 const submitForm = (event) => {
     event.preventDefault();
@@ -53,20 +66,23 @@ const submitForm = (event) => {
         Title: document.getElementById('title').value,
         Content: document.getElementById('content').value
     }
+    // Here we are creating a form object and setting its username title and content properties
+        // to be equal to those that were entered by the user    
     forms.push(form);
+        // This pushes each new form is in the forms array    
     checkAllBoxes();
+    // This runs the function that checks to make sure that all the fields are filled out    
     document.querySelector('form').reset();
-
+ // This resets the form so that more submissions can be made    
     localStorage.setItem('formSubmissionsList', JSON.stringify(forms));
-
+// This turns the submission into a string so that it can be stored in the local storage    
     window.location.href="blog.html"
     //window.location.href="posts.html"
-    
+    // this directs us to the blog page when the submission is shown
 }
 //console.log(forms)
 document.getElementById('submitButton').addEventListener('click', submitForm);
-
-
+// when the submit button is clicked it runs the submit form function
     
 
 
